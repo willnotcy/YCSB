507698b55506b039b7d12b4d600b8456c147d92f
mark kristensen <markkristensen@outlook.com>
2021-09-12 21:29:40 +0200

    DisadvantageGenerator

---
+++ core/src/main/java/site/ycsb/generator/DisadvantageGenerator.java
@@ -0,0 +1,33 @@
+package site.ycsb.generator;
+import java.util.Random;
+
+/**
+ * Generates a sequence of values favoring lower value items.
+ */
+public class DisadvantageGenerator extends NumberGenerator {
+
+    public DisadvantageGenerator() {
+        
+    }
+
+    /**
+     * Generate the next value in the distribution.
+     */
+    @Override
+    public Long nextValue() {
+        Random rand = new Random();
+
+        long rand1 = rand.nextLong();
+        long rand2 = rand.nextLong();
+        long rand3 = rand.nextLong();
+        long rand4 = rand.nextLong();
+        long rand5 = rand.nextLong();
+
+        return min(rand1, rand2, rand3, rand4, rand5);
+    }
+
+    private static long min(long a, long b, long c, long d, long e) {
+        return Math.min(a, Math.min(b, Math.min(c, Math.min(d, e))));
+    }
+  }
+  
--- core/src/main/java/site/ycsb/workloads/CoreWorkload.java
+++ core/src/main/java/site/ycsb/workloads/CoreWorkload.java
@@ -526,6 +526,9 @@ public class CoreWorkload extends Workload {
           Double.parseDouble(p.getProperty(HOTSPOT_OPN_FRACTION, HOTSPOT_OPN_FRACTION_DEFAULT));
       keychooser = new HotspotIntegerGenerator(insertstart, insertstart + insertcount - 1,
           hotsetfraction, hotopnfraction);
+    } 
+    else if (requestdistrib.equals("disadvantage")){
+      keychooser = new DisadvantageGenerator();
     } else {
       throw new WorkloadException("Unknown request distribution \"" + requestdistrib + "\"");
     }
